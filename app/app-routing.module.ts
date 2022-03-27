5c9227800514b35ae8e46f38587111e5d6cda4fc
thornpw <thorsten.butschke@googlemail.com>
2022-03-27 13:43:19 +0200

    1 file modified, 3 deleted

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,22 +1,19 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {FirstComponent} from './first/first.component';
-import {SecondComponent} from './second/second.component';
-import {ThirdComponent} from './third/third.component';
-import {FourthComponent} from './fourth/fourth.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
 const routes: Routes = [
-  {path: '', component: FirstComponent, pathMatch: 'full'},
-  {path: 'material-design', component: FirstComponent},
-  {path: 'what-up-web', component: SecondComponent},
-  {path: 'my-ally-cli', component: ThirdComponent},
-  {path: 'become-angular-tailer', component: FourthComponent}
+  { path: '', component: FirstComponent, pathMatch: 'full' },
+  { path: 'material-design', component: FirstComponent },
+  { path: 'what-up-web', component: SecondComponent },
+  { path: 'my-ally-cli', component: ThirdComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: []
+  providers: [],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
